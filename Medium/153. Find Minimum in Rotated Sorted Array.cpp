2b67d9649957908ca88cbb5a56cd0e4d2202b045
= <=>
2025-06-26 19:54:09 +0300

    binary search

---
+++ Medium/153. Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Belal on 26-Jun-25.
+//
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+};
